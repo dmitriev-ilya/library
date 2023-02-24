d446bd6f75c6c68bb7f7656cd1c4d8fff42d7550
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-02-24 19:22:59 +0500

    download books with titles

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-os.makedirs("books", exist_ok=True)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
 
-for id in range(1, 11):
-    url = f"https://tululu.org/txt.php?id={id}"
+
+def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
 
-    filename = os.path.join("books", f"id{id}.txt")
-    with open(filename, 'wb') as file:
+    os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    with open(filepath, 'wb') as file:
         file.write(response.content)
+    return filepath
+
+
+for id in range(1, 11):
+    try:
+        download_url = f"https://tululu.org/txt.php?id={id}"
+        book_url = f"https://tululu.org/b{id}/"
+        response = requests.get(book_url)
+        response.raise_for_status()
+        check_for_redirect(response)
+
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_with_author = soup.find('table').find('h1').text
+        title = title_with_author.split('::')[0].strip()
+        author = title_with_author.split('::')[1].strip()
+
+        filename = f"{id}. {title}.txt"
+        download_txt(download_url, filename)
+    except requests.HTTPError:
+        print('Book not found')
