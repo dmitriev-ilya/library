77253a1e107708095ef793395dba2e0874433461
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-02-24 19:51:12 +0500

    download book covers.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import urllib
 
 import requests
 from bs4 import BeautifulSoup
@@ -21,7 +22,19 @@ def download_txt(url, filename, folder='books/'):
         file.write(response.content)
     return filepath
 
+def download_image(url, folder='images/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
 
+    os.makedirs(folder, exist_ok=True)
+    filename = urllib.parse.urlsplit(url)[2].split('/')[-1]
+    filepath = os.path.join(folder, urllib.parse.unquote(filename))
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+"""
 for id in range(1, 11):
     try:
         download_url = f"https://tululu.org/txt.php?id={id}"
@@ -37,5 +50,21 @@ for id in range(1, 11):
 
         filename = f"{id}. {title}.txt"
         download_txt(download_url, filename)
+    except requests.HTTPError:
+        print('Book not found')
+"""
+
+for id in range(1, 11):
+    try:
+        book_url = f"https://tululu.org/b{id}/"
+        response = requests.get(book_url)
+        response.raise_for_status()
+        check_for_redirect(response)
+
+        soup = BeautifulSoup(response.text, 'lxml')
+        post_img_url = soup.find('div', class_='bookimage').find('img')['src']
+        absolute_img_url = urllib.parse.urljoin('https://tululu.org/',post_img_url)
+        download_image(absolute_img_url)
+
     except requests.HTTPError:
         print('Book not found')
