e9ffd3090138039572eac07710a732dec042c1f4
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-03-29 22:07:49 +0500

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -13,14 +14,23 @@ with open("books.json", "r") as file:
     books_json = file.read()
 
 books = json.loads(books_json)
-books_on_couples = list(chunked(books, 2))
+books_by_pages = list(chunked(books, 10))
+
+
+def render_template(books_by_pages, pages_path='pages'):
+    os.makedirs(pages_path, exist_ok=True)
+
+    for page_number, books_on_page in enumerate(books_by_pages, start=1):
+        books_on_couples = list(chunked(books_on_page, 2))
+        template = env.get_template('template.html')
+        rendered_page = template.render(books_on_couples=books_on_couples)
+        filepath = os.path.join(pages_path, f'index{page_number}.html')
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def on_reload():
-    template = env.get_template('template.html')
-    rendered_page = template.render(books_on_couples=books_on_couples)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    render_template(books_by_pages)
     print("Site rebuilted")
 
 
