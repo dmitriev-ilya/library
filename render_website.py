dd2c566ea7bf9fdf81248dab6ba365ec33cdaffd
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-04 22:02:32 +0500

    variable names fixed

--- render_website.py
+++ render_website.py
@@ -34,20 +34,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with open(args.json_path, 'r') as file:
-        books = json.load(file)
+        books_description = json.load(file)
 
-    books_by_pages = list(chunked(books, 10))
+    books_description_by_pages = list(chunked(books_description, 10))
 
-    def render_template(books_by_pages, pages_path=args.pages_path):
+    def render_template(books_description_by_pages, pages_path=args.pages_path):
         os.makedirs(pages_path, exist_ok=True)
-        pages_number_range = list(range(1, len(books_by_pages) + 1))
-        for page_number, books_on_page in enumerate(books_by_pages, start=1):
-            books_on_couples = list(chunked(books_on_page, 2))
+        pages_number_range = list(range(1, len(books_description_by_pages) + 1))
+        for page_number, books_on_page in enumerate(books_description_by_pages, start=1):
+            books_description_on_couples = list(chunked(books_on_page, 2))
             template = env.get_template('template.html')
             index_filepath = os.path.join(pages_path, f'index{page_number}.html')
 
             rendered_page = template.render(
-                books_on_couples=books_on_couples,
+                books_description_on_couples=books_description_on_couples,
                 pages_number_range=pages_number_range,
                 current_page=page_number
             )
@@ -55,11 +55,11 @@ if __name__ == '__main__':
                 file.write(rendered_page)
 
     def on_reload():
-        render_template(books_by_pages)
+        render_template(books_description_by_pages)
         print('Site rebuilted')
 
     on_reload()
 
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='.', default_filename='pages/index1.html')
+    server.serve(root='.', default_filename=os.path.join(args.pages_path, 'index1.html'))
