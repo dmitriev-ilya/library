fa0421fd59a8b53e0f0e42f059cc0d8ad6e90601
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-04 21:48:03 +0500

    argparse added

--- render_website.py
+++ render_website.py
@@ -1,47 +1,66 @@
 import json
 import os
+import argparse
 
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-with open('media/books.json', 'r') as file:
-    books_json = file.read()
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-books = json.loads(books_json)
-books_by_pages = list(chunked(books, 10))
+    parser = argparse.ArgumentParser(
+        description='Скрипт для запуска сервера и рендеринга сайта  \
+        с книгами научной фантастики'
+    )
+    parser.add_argument(
+        '-j',
+        '--json_path',
+        help='путь к файлу .json для c информацией о книгах',
+        default='media/books.json',
+        type=str
+    )
+    parser.add_argument(
+        '-p',
+        '--pages_path',
+        help='путь к каталогу для хранения срендеренных страниц сайта',
+        default='pages',
+        type=str
+    )
+    args = parser.parse_args()
 
+    with open(args.json_path, 'r') as file:
+        books_json = file.read()
 
-def render_template(books_by_pages):
-    pages_path = 'pages'
-    os.makedirs(pages_path, exist_ok=True)
-    pages_number_range = list(range(1, len(books_by_pages) + 1))
-    for page_number, books_on_page in enumerate(books_by_pages, start=1):
-        books_on_couples = list(chunked(books_on_page, 2))
-        template = env.get_template('template.html')
-        index_filepath = os.path.join(pages_path, f'index{page_number}.html')
+    books = json.loads(books_json)
+    books_by_pages = list(chunked(books, 10))
 
-        rendered_page = template.render(
-            books_on_couples=books_on_couples,
-            pages_number_range=pages_number_range,
-            current_page=page_number
-        )
-        with open(index_filepath, 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+    def render_template(books_by_pages, pages_path=args.pages_path):
+        os.makedirs(pages_path, exist_ok=True)
+        pages_number_range = list(range(1, len(books_by_pages) + 1))
+        for page_number, books_on_page in enumerate(books_by_pages, start=1):
+            books_on_couples = list(chunked(books_on_page, 2))
+            template = env.get_template('template.html')
+            index_filepath = os.path.join(pages_path, f'index{page_number}.html')
 
+            rendered_page = template.render(
+                books_on_couples=books_on_couples,
+                pages_number_range=pages_number_range,
+                current_page=page_number
+            )
+            with open(index_filepath, 'w', encoding='utf8') as file:
+                file.write(rendered_page)
 
-def on_reload():
-    render_template(books_by_pages)
-    print('Site rebuilted')
+    def on_reload():
+        render_template(books_by_pages)
+        print('Site rebuilted')
 
+    on_reload()
 
-on_reload()
-
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.', default_filename='pages/index1.html')
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.', default_filename='pages/index1.html')
